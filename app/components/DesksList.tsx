86e70dffe4c4c4f4ca546061a6bc6f8db3ebbb68
carlijnweijer <carlijnweijer@gmail.com>
2020-11-20 12:39:22 +0100

    Add sort by distance

--- app/components/DesksList.tsx
+++ app/components/DesksList.tsx
@@ -1,4 +1,4 @@
-import React, { useState } from "react";
+import React, { useEffect, useState } from "react";
 import {
   View,
   Image,
@@ -13,11 +13,44 @@ import {
 import { usePaginatedQuery } from "react-query";
 import { useNavigation } from "@react-navigation/native";
 
+import * as Location from "expo-location";
+import getDistance from "geolib/es/getDistance";
+
 import { fetchDesksList, DesksListResult, DeskResult } from "app/lib/api";
 import { Pill } from "app/ui";
+import { LocationObject } from "expo-location";
 
 export function DesksList() {
   const [filter, setFilter] = useState(0);
+
+  const [location, setLocation] = useState<LocationObject | null>(null);
+  const [errorMsg, setErrorMsg] = useState<
+    "Permission to access location was denied" | null
+  >(null);
+
+  useEffect(() => {
+    (async () => {
+      let { status } = await Location.requestPermissionsAsync();
+      if (status !== "granted") {
+        setErrorMsg("Permission to access location was denied");
+      }
+
+      let location = await Location.getCurrentPositionAsync({});
+      setLocation(location);
+      console.log(
+        "what is location ",
+        location.coords.latitude,
+        location.coords.longitude
+      );
+    })();
+  }, [filter]);
+
+  let text = "Waiting..";
+  if (errorMsg) {
+    text = errorMsg;
+  } else if (location) {
+    text = JSON.stringify(location);
+  }
 
   const {
     resolvedData: list,
@@ -36,32 +69,67 @@ export function DesksList() {
     Alert.alert(`${error}`);
   }
 
-  let sortedList = list ? [...list.results ] : undefined;
+  let sortedList = list ? [...list.results] : undefined;
   if (filter === 0) {
-    sortedList?.sort((a, b) => new Date(b.createdAt).getTime() - new Date(a.createdAt).getTime())
+    sortedList?.sort(
+      (a, b) =>
+        new Date(b.createdAt).getTime() - new Date(a.createdAt).getTime()
+    );
+  } else if (filter === 2 && location) {
+    /* 
+    sort by location nearest to me
+    for every desk you need the distance, then sort by distance and give back sorted array
+    object.latitude
+    object.longitude
+    */
+    sortedList?.sort(
+      (a, b) =>
+        getDistance(
+          { latitude: a.latitude, longitude: a.longitude },
+          {
+            latitude: location.coords.latitude,
+            longitude: location.coords.longitude,
+          }
+        ) -
+        getDistance(
+          { latitude: b.latitude, longitude: b.longitude },
+          {
+            latitude: location.coords.latitude,
+            longitude: location.coords.longitude,
+          }
+        )
+    );
+    console.log("sorted list is now ", sortedList);
   }
-  
+  // console.log("what is sortedList", sortedList);
+  // const distance = getDistance(
+  //   { latitude: 49.3880614, longitude: 3.823527 },
+  //   { latitude: 50.3880614, longitude: 4.023527 }
+  // );
+
+  // console.log("what is feedback getDistance", distance);
+  console.log("what is filter", filter);
 
   return (
     <View style={{ flex: 1 }}>
       <View style={styles.filters}>
         <View style={{ marginRight: 8 }}>
           <Pill
-            text='Newest'
+            text="Newest"
             selected={filter === 0}
             onPress={() => setFilter(0)}
           />
         </View>
         <View style={{ marginRight: 8 }}>
           <Pill
-            text='Trending'
+            text="Trending"
             selected={filter === 1}
             onPress={() => setFilter(1)}
           />
         </View>
         <View style={{ marginRight: 8 }}>
           <Pill
-            text='Near me'
+            text="Near me"
             selected={filter === 2}
             onPress={() => setFilter(2)}
           />
@@ -72,7 +140,7 @@ export function DesksList() {
           <RefreshControl refreshing={isFetching} onRefresh={refetch} />
         }
         data={sortedList}
-        keyExtractor={desk => `${desk.id}`}
+        keyExtractor={(desk) => `${desk.id}`}
         renderItem={({ item: desk }) => <DeskItem desk={desk} />}
       />
     </View>
